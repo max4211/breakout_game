0eeee218be70755b8e8fdb05226f1471e5530b81
Max Smith <ms724@duke.edu>
2020-01-16 15:27:39 -0500

    Added stupid (no collide/strength) brick

---
+++ doc/TODO.md
@@ -0,0 +1,11 @@
+# TODO
+## Max Smith
+
+#### Changes to make
+- Make global sizing variables dynamic to game screen
+- Make brick sizing dynamic for screen
+
+#### Next steps
+- Create and experiment with bricks
+- Collision detection (NVIDIA, n^2 algo efficient up to 10E6 objects)
+- Divide code up into classes
--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -26,21 +26,35 @@ import java.lang.Math.*;
  * @author Max Smith
  */
 public class Main extends Application {
+    // Game metadata
     public static final String TITLE = "Bounce Test JavaFX";
     public static final int SIZE = 400;
     public static final int FRAMES_PER_SECOND = 120;
     public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
     public static final Paint BACKGROUND = Color.AZURE;
+
+    // Brick metadata
+    public static final int BRICK_ROWS = 25;
+    public static final int BRICKS_PER_ROW = 10;
+    public static final int BRICK_TOP_PAD = 25;
+    public static final int BRICK_FLOOR_PAD = 40;
+    public static final int BRICK_RIGHT_PAD = 15;
+    public static final int BRICK_LEFT_PAD = 15;
+
+    // Wall metadata
     public static final int WALL_FLOAT = 15;
     public static final int WALL_WIDTH = 10;
+    public static final Paint WALL_FILL = Color.BLACK;
+
+    // Paddle metadata
     public static final int PADDLE_HEIGHT = 10;
     public static final int PADDLE_SPEED = 10;
     public static final int PADDLE_FLOAT = 30;              // paddle pixel float above bottom of screen
     public static final double PADDLE_EDGE = Math.PI / 4;   // max paddle angular deflection off edge
     public static final Paint PADDLE_COLOR = Color.BLUEVIOLET;
     public static final Paint BOUNCER_COLOR = Color.GOLD;
-    public static final Paint WALL_FILL = Color.BLACK;
+
 
     // idea to handle ball movement, set angle of motion (theta) and speed
     // these should be sufficient to handle the ball vector
@@ -52,6 +66,11 @@ public class Main extends Application {
     public static int PADDLE_WIDTH = 100;
     public static int LIVES_LEFT = 3;
     public static boolean BALL_STUCK = false;
+
+    // dynamic variable sizes to configure at start of game
+    public static double BRICK_HEIGHT;
+    public static double BRICK_WIDTH;
+    public static final Paint BRICK_COLOR = Color.RED;
 
     // some things needed to remember during game
     private Scene myScene;
@@ -90,18 +109,40 @@ public class Main extends Application {
         myLeftWall = createWall(WALL_FLOAT, WALL_FLOAT, WALL_WIDTH, height - WALL_FLOAT);
         myRightWall = createWall(width - 2 * WALL_FLOAT, WALL_FLOAT, WALL_WIDTH, height - WALL_FLOAT);
         myTopWall = createWall(WALL_FLOAT, WALL_FLOAT, width - 3 * WALL_FLOAT, WALL_WIDTH);
+        createBrick(width, height);
 
         root.getChildren().add(myBouncer);
         root.getChildren().add(myPaddle);
         root.getChildren().add(myLeftWall);
         root.getChildren().add(myRightWall);
         root.getChildren().add(myTopWall);
+        root.getChildren().add(myBrick);
 
         // create a place to see the shapes
         Scene scene = new Scene(root, width, height, background);
         // respond to input
         scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
         return scene;
+    }
+
+    // TODO: Incorporate padding to decrease dimensions
+    private void setBrickDimensions() {
+        double leftX = myLeftWall.getX() + myLeftWall.getWidth();
+        double rightX = myRightWall.getX();
+        double bottomY = myPaddle.getY();
+        double topY = myTopWall.getY() + myTopWall.getHeight();
+        BRICK_WIDTH = (rightX - leftX) / BRICKS_PER_ROW;
+        BRICK_HEIGHT = (bottomY - topY) / BRICK_ROWS;
+    }
+
+    private void createBrick(int width, int height) {
+        setBrickDimensions();
+        myBrick = new Rectangle();
+        myBrick.setX(width / 2 - myBrick.getBoundsInLocal().getWidth() / 2);
+        myBrick.setY(height / 2 - myBrick.getBoundsInLocal().getHeight() / 2);
+        myBrick.setWidth(BRICK_WIDTH);
+        myBrick.setHeight(BRICK_HEIGHT);
+        myBrick.setFill(BRICK_COLOR);
     }
 
     private void createPaddle(int width, int height) {
